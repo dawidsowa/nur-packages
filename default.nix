28cc74df280c93462df46023ae20861301833096
dawidsowa <dawid_sowa@posteo.net>
2020-09-21 10:18:32 +0200

    reddit-top-rss: init

--- default.nix
+++ default.nix
@@ -6,16 +6,13 @@
 # commands such as:
 #     nix-build -A mypackage
 
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 {
   # The `lib`, `modules`, and `overlay` names are special
-  lib = import ./lib { inherit pkgs; }; # functions
-  modules = import ./modules; # NixOS modules
-  overlays = import ./overlays; # nixpkgs overlays
+  # lib = import ./lib { inherit pkgs; }; # functions
+  # modules = import ./modules; # NixOS modules
+  # overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  reddit-top-rss = pkgs.callPackage ./pkgs/reddit-top-rss { };
 }
-
