fa334d1b33eab0b535ae026340fa226392f80fe2
Dawid Sowa <dawid_sowa@posteo.net>
2020-10-11 19:36:17 +0200

    gnome3: module init

--- hm-modules/default.nix
+++ hm-modules/default.nix
@@ -2,4 +2,5 @@
   xcompose = ./services/xcompose.nix;
   gnome-terminal = ./programs/gnome-terminal.nix;
   merge = ./merge.nix;
+  gnome3 = ./services/gnome.nix;
 }
---
+++ hm-modules/services/gnome.nix
@@ -0,0 +1,38 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let
+  cfg = config.gnome3;
+
+  extensionsUuids = map (x: if (strings.isStorePath x) then x.uuid else x) cfg.extensions;
+  extensionsPackages = builtins.filter strings.isStorePath cfg.extensions;
+
+  mapGnomeSettings = n: v: nameValuePair ("org/gnome/" + n) v;
+in
+{
+  options = {
+    gnome3 = {
+      extensions = mkOption {
+        description = "List of Gnome extensions to be installed and enabled.";
+        type = with types; listOf (either package str);
+        default = [ ];
+        example = ''with pkgs.gnomeExtensions; [
+            dash-to-panel
+            arc-menu
+        ]
+        '';
+      };
+      settings = mkOption {
+        description = "Gnome settings";
+        default = { };
+      };
+    };
+  };
+  config = {
+    home.packages = extensionsPackages;
+
+    dconf.settings = {
+      "org/gnome/shell".enabled-extensions = extensionsUuids;
+    } // (mapAttrs' mapGnomeSettings cfg.settings);
+  };
+}
